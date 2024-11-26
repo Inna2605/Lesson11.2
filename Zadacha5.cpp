f3ccd6a3b87ba6b8f174b7e0093a0db8ba55dfaa
Inna <prizrak1609@gmail.com>
2024-11-26 19:30:26 +0200

    Zrobleno chastkovo rekyrsiy

--- Zadacha5.cpp
+++ Zadacha5.cpp
@@ -4,25 +4,23 @@
 Після відгадування числа на екран необхідно вивести кількість зроблених користувачем спроб.
 У програмі необхідно використовувати рекурсію.*/
 #include "Zadacha5.h"
+#include "Windows.h"
 using namespace std;
 void Zagadane_chislo(int Ar[], int N);
-void Show_chisla(int Ar[], int N);
 int Buku(int Ar[], int N, int C, int b);
 int Korovu(int Ar[], int N, int C, int k);
-int Chislo(int Ar[], int N);
-int Porivnanna(int Ar[], int N, int x, int b);
+int Chislo(int Ar[], int N, int Ch, int R);
 
 void Gra()
 {
+	Random();
 	const int N = 4;
 	int Ar[N];
-
+	int Ch = 0, R = 1;
 	cout << "Maemo zagadane chislo: ";
 	Zagadane_chislo(Ar, N);
-
-	int K = Chislo(Ar, N);
-
-	Show_chisla(Ar, N);
+	int K = Chislo(Ar, N, Ch, R);
+	Show(K);
 	Show('\n');
 	while(true) {
 		int b = 0, k = 0;
@@ -38,43 +36,28 @@ void Gra()
 		Show(k);
 		Show('\n');
 		Show('\n');
-		if (K == a)break;
+		if (K == a)return;
 	}
 }
 
 void Zagadane_chislo(int Ar[], int N)
 {
-	int i = 0;
-	Random();
-	while (i < N) {
-		if (i == N)break;
-		Ar[i] = rand() % 8 + 1;
-		i++;
-	}
-}
-
-void Show_chisla(int Ar[], int N)
-{
-	int i = 0;
-	while (true) {
-		if (i == N)break;
-		Show(Ar[i]);
-		i++;
-	}
-	Show('\n');
+	if (N == 0)return;
+	Ar[N - 1] = rand() % 8 + 1;
+	Zagadane_chislo(Ar, N-1);
 }
 
 int Buku(int Ar[], int N, int C, int b)
 {
-	int x, y = C, i = 0, k = 0, n = 0;
+	int x, i = 0, k = 0, n = 0;
 	const int N2 = 4;
 	int Ar2[N2];
 	
 
 	while (true) {
 		if (i == N)break;
-		x = y % 10;
-		y /= 10;
+		x = C % 10;
+		C /= 10;
 		int r = 0;
 		while (true) {
 			if (r == N2)break;
@@ -114,29 +97,9 @@ int Korovu(int Ar[], int N, int C, int k)
 	return Korovu(Ar, N, C /= 10, k);
 }
 
-int Chislo(int Ar[], int N)
+int Chislo(int Ar[], int N, int Ch, int R)
 {
-	int K = 1000, Sum = 0, C = 0, i = 0;
-	while (true) {
-		if (i == N)break;
-		C = Ar[i] * K;
-		Sum += C;
-		K /= 10;
-		i++;
-	}
-	return Sum;
-}
-
-int Porivnanna(int Ar[], int N, int x, int b)
-{
-	int j = 0;
-	while (true) {
-		if (j == N - 1)break;
-		if (x == Ar[j]) {
-			b++;
-			return b;
-		}
-		j++;
-	}
-	return b;
+	if (N == 0)return Ch;
+	Ch += Ar[N - 1] * R;
+	return Chislo(Ar, N - 1, Ch, R * 10);
 }
